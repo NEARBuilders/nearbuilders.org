f5e59ebbe096f5cdd249302f11e8e834c46db51c
Zeeshan Ahmad <itexpert120@outlook.com>
2026-06-15 19:32:57 +0500

    format

--- ui/src/routes/_layout/builders/add.tsx
+++ ui/src/routes/_layout/builders/add.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-
 export const Route = createFileRoute("/_layout/builders/add")({
   head: () => ({
     meta: [
