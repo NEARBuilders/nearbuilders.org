eaab966f4facfc86fc5620de2d2e675702e49af8
Elliot Braem <elliot@ejlbraem.com>
2026-07-27 01:17:47 -0500

    fix sql

--- plugins/projects/src/db/migrations/0002_lowly_darkstar.sql
+++ plugins/projects/src/db/migrations/0002_lowly_darkstar.sql
@@ -1,9 +1,5 @@
-DO $$ BEGIN
-  IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'project_apps' AND column_name = 'gateway_id') THEN
-    ALTER TABLE "project_apps" RENAME COLUMN "gateway_id" TO "domain";
-  END IF;
-END $$;
---> statement-breakpoint
+ALTER TABLE "project_apps" ADD COLUMN IF NOT EXISTS "domain" text NOT NULL;--> statement-breakpoint
 DROP INDEX IF EXISTS "project_app_unique";--> statement-breakpoint
 CREATE UNIQUE INDEX IF NOT EXISTS "project_app_unique" ON "project_apps" USING btree ("project_id","account_id","domain");--> statement-breakpoint
+ALTER TABLE "project_apps" DROP COLUMN IF EXISTS "gateway_id";--> statement-breakpoint
 ALTER TABLE "project_apps" DROP COLUMN IF EXISTS "position";
